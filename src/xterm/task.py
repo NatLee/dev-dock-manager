e2b502d1a47c1db38815739367d93cb4c58c7a3e
NatLee <natlee.work@gmail.com>
2023-11-14 11:10:41 +0800

    [update] add `traefik` labels & attach to network

--- src/xterm/task.py
+++ src/xterm/task.py
@@ -5,6 +5,7 @@ from django_rq import job
 def run_image_task(image_name, ports, volumes, environment, name, privileged=False, nvdocker=False):
     client = docker.from_env()
     device_requests = []
+    network = client.networks.get('d-gui-network')
 
     if nvdocker:
         # Define device requests for NVIDIA GPUs
@@ -16,6 +17,14 @@ def run_image_task(image_name, ports, volumes, environment, name, privileged=Fal
             )
         ]
 
+    traefik_labels = {
+        "traefik.enable": "true",
+        f"traefik.http.routers.d-gui-{name}.rule": f"PathPrefix(`/novnc/{name}/`)",
+        f"traefik.http.services.d-gui-{name}.loadbalancer.server.port": "6901",
+        f"traefik.http.middlewares.d-gui-{name}-strip-prefix.stripprefix.prefixes": f"/novnc/{name}/",
+        f"traefik.http.routers.d-gui-{name}.middlewares": f'd-gui-{name}-strip-prefix',
+    }
+
     container = client.containers.run(
         image_name,
         stdin_open=True,
@@ -26,7 +35,9 @@ def run_image_task(image_name, ports, volumes, environment, name, privileged=Fal
         environment=environment,
         name=name,
         privileged=privileged,
-        device_requests=device_requests
+        device_requests=device_requests,
+        network=network.id,  # Attach the container to the network
+        labels=traefik_labels
     )
 
     image_name = "none"
