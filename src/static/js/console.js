3c71172086e14baf5a69aa32fc9ba8a547a8fd89
NatLee <natlee.work@gmail.com>
2024-04-19 00:36:54 +0800

    [update] add verification with token in WS

--- src/static/js/console.js
+++ src/static/js/console.js
@@ -48,8 +48,9 @@ async function loadConsoleData(containerID, action) {
 function setupWebSocketConnection(containerID, action){
     Terminal.applyAddon(fit);
 
+    const accessToken = localStorage.getItem('accessToken');
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var ws_path = ws_scheme + '://' + window.location.host + "/ws/console/";
+    var ws_path = ws_scheme + '://' + window.location.host + `/ws/console/?token=${accessToken}`;
     var socket = new WebSocket(ws_path);
 
     // Convert and send a message to the server
@@ -94,7 +95,7 @@ function setupWebSocketConnection(containerID, action){
         console.log("WebSocket connection established");
         // Handle the connect event
         status.innerHTML = '<span style="background-color: lightgreen;">connected</span>';
-        // Here we assume `action` and `containerID` are defined elsewhere in your script
+        // Send the container ID to the server
         sendWebSocketMessage(action, { "Id": containerID });
         term.focus();
     };
